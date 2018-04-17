675722582cf6a5b2966aebf1394724d75b0e39ce
Mark Tripolt <mark@tripolt.de>
2018-04-17 10:46:16 +0200

    added several tests for neighbours count

--- test/testPlayGround.cpp
+++ test/testPlayGround.cpp
@@ -15,7 +15,6 @@ TEST(PlayGroundTest, shouldReturnThreeNeigboursOnTopRightPositionWithEmptyPlayGr
     for(int i = 0; i<expectedNeigbours.size();i++){
         ASSERT_EQ(expectedNeigbours[i],haveNeighbours[i]);
     }
-
 }
 
 
@@ -28,5 +27,48 @@ TEST(PlayGroundTest, shouldReturnThreeNeigboursOnTopLeftPosstionWithEmptyPlayGro
     for(int i = 0; i<expectedNeigbours.size();i++){
         ASSERT_EQ(expectedNeigbours[i],haveNeighbours[i]);
     }
+}
 
+TEST(PlayGroundTest, shouldReturnFiveNeigboursOnTopMiddlePostionWithEmptyPlayGround) {
+    vector<int> expectedNeigbours = {0,2,4,5,6};
+    auto *gen = new PlayGround(4, 1);
+    gen->generateField();
+    vector<int> haveNeighbours = gen->getNeighbours();
+    ASSERT_EQ(expectedNeigbours.size(),haveNeighbours.size());
+    for(int i = 0; i<expectedNeigbours.size();i++){
+        ASSERT_EQ(expectedNeigbours[i],haveNeighbours[i]);
+    }
+}
+
+TEST(PlayGroundTest, shouldReturnFiveNeigboursOnBottomMiddlePostionWithEmptyPlayGround) {
+    vector<int> expectedNeigbours = {8,9,10,12,14};
+    auto *gen = new PlayGround(4, 13);
+    gen->generateField();
+    vector<int> haveNeighbours = gen->getNeighbours();
+    ASSERT_EQ(expectedNeigbours.size(),haveNeighbours.size());
+    for(int i = 0; i<expectedNeigbours.size();i++){
+        ASSERT_EQ(expectedNeigbours[i],haveNeighbours[i]);
+    }
+}
+
+TEST(PlayGroundTest, shouldReturnFiveNeigboursOnMiddleRightPostionWithEmptyPlayGround) {
+    vector<int> expectedNeigbours = {2,3,6,10,11};
+    auto *gen = new PlayGround(4, 7);
+    gen->generateField();
+    vector<int> haveNeighbours = gen->getNeighbours();
+    ASSERT_EQ(expectedNeigbours.size(),haveNeighbours.size());
+    for(int i = 0; i<expectedNeigbours.size();i++){
+        ASSERT_EQ(expectedNeigbours[i],haveNeighbours[i]);
+    }
+}
+
+TEST(PlayGroundTest, shouldReturnFiveNeigboursOnMiddleLeftPostionWithEmptyPlayGround) {
+    vector<int> expectedNeigbours = {0,1,5,8,9};
+    auto *gen = new PlayGround(4, 4);
+    gen->generateField();
+    vector<int> haveNeighbours = gen->getNeighbours();
+    ASSERT_EQ(expectedNeigbours.size(),haveNeighbours.size());
+    for(int i = 0; i<expectedNeigbours.size();i++){
+        ASSERT_EQ(expectedNeigbours[i],haveNeighbours[i]);
+    }
 }
