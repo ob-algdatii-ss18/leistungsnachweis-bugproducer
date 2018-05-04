7423c4563ae69afe70696a32b893d82f815ce1c4
Johdeath <johannes.arzt@gmx.net>
2018-05-04 11:19:14 +0200

    refactoring calcNeighbours

--- hidato/PlayGround.h
+++ hidato/PlayGround.h
@@ -17,12 +17,13 @@ public:
     int runCounter;
 
     std::vector<std::vector<int>> getPlayGround();
-    std::vector<int> calcNeighbours(int);
+    std::vector<int> calcNeighbours(int, bool);
 
     const std::vector<int> &getNeighbours() const;
     void fillPlayGround();
     void printPlayGrounds();
     void generateUnsolvedPlayground();
+    void setPlayGroundSolved(const std::vector<std::vector<int>> &playGroundSolved);
 
 private:
 
@@ -35,7 +36,6 @@ private:
     std::vector<int> neighbours;
     std::vector<int> numberAddresses;
 
-
     unsigned int fieldSize;
     int currentPosition;
     int isRandom;
@@ -43,23 +43,26 @@ private:
 
     bool checkForSameNeighbours(int, int);
 
+public:
+    void setCurrentPosition(int currentPosition);
 
+private:
 
-    void hasRightNeighbour(std::vector<int> &neighbours, int) const;
+    void hasRightNeighbour(std::vector<int> &neighbours, bool) const;
 
-    void hasBottomNeighbour(std::vector<int> &neighbours, int) const;
+    void hasBottomNeighbour(std::vector<int> &neighbours, bool) const;
 
-    void hasBottomRightNeighbour(std::vector<int> &neighbours, int) const;
+    void hasBottomRightNeighbour(std::vector<int> &neighbours, bool) const;
 
-    void hasLeftNeighbour(std::vector<int> &neighbours, int) const;
+    void hasLeftNeighbour(std::vector<int> &neighbours, bool) const;
 
-    void hasBottomLeftNeighbour(std::vector<int> &neighbours, int) const;
+    void hasBottomLeftNeighbour(std::vector<int> &neighbours, bool) const;
 
-    void hasTopNeighbour(std::vector<int> &neighbours, int) const;
+    void hasTopNeighbour(std::vector<int> &neighbours, bool) const;
 
-    void hasTopRightNeighbour(std::vector<int> &neighbours, int) const;
+    void hasTopRightNeighbour(std::vector<int> &neighbours, bool) const;
 
-    void hasTopLeftNeighbour(std::vector<int> &neighbours, int) const;
+    void hasTopLeftNeighbour(std::vector<int> &neighbours, bool) const;
 
     bool checkDensity();
 };
