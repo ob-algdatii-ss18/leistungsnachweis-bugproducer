b85adb7238a538b31da3a5a8ffb023a8f7d1e854
isigoing <tripolt@hm.edu>
2018-05-30 15:10:06 +0200

    added another counter

--- test/testPlayGround.cpp
+++ test/testPlayGround.cpp
@@ -344,14 +344,16 @@ TEST(PlayGroundTest, shouldGenerateValidUnsolvedPlayGround) {
 
 
 //TEST(PlayGroundTest, test) {
+//    int count= 0;
 //
 //    for(int i = 0; i < 100; i++) {
+//        cout << "Field Number: " << count << endl;
 //        cout << "\n" << endl;
 //        auto *gen = new PlayGround(6, -1);
 //        gen->generateField();
 //        gen->fillPlayGround();
 //        gen->printPlayGrounds();
-//
+//        count++;
 //
 //   }
 //
